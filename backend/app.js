d3f6bc08487b78bda96892eef3bd62bdcd17b46f
Jordyn Lally <codingwithjordyn@gmail.com>
2024-02-19 17:18:35 -0500

    hooked mongodb database, created blogs collection

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog.js');
 
 const app = express();
+
 app.set('view engine', 'ejs');
 app.engine('ejs', require('ejs').__express);
 app.set('views', 'frontend');
-app.listen(4000);
+const dbURI = 'mongodb+srv://codingwithjordyn:4qk2LnPqnug5u9OG@learning-mern.aqkcpuk.mongodb.net/learning-mern?retryWrites=true&w=majority'
+mongoose.connect(dbURI)
+    .then((result) => app.listen(4000))
+    .catch((err) => console.log(err));
 
-app.use('/frontend', express.static('frontend'));
+app.use('/public', express.static('public'));
+
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'new blog',
+        snippet: 'about new blog',
+        body: 'more about new blog'
+    })
+
+    blog.save()
+        .then((result) => res.send(result))
+        .catch((err) => console.log(err));
+});
 
 app.get('/', (req, res) => {
     const blogs = [
