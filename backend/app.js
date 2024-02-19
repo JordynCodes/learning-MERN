c9e08fa0eefa02f9af609c66096ca04c0ed2e90d
Jordyn Lally <codingwithjordyn@gmail.com>
2024-02-19 12:17:02 -0500

    added partial files, display blogs list

--- backend/app.js
+++ backend/app.js
@@ -7,17 +7,22 @@ app.set('views', 'frontend');
 app.listen(4000);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const blogs = [
+        {title: 'Blog number 1', snippet: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'},
+        {title: 'Blog number 2', snippet: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'},
+        {title: 'Blog number 3', snippet: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'},
+    ];
+    res.render('index', {title: 'Home', blogs});
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', {title: 'About'}, );
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', {title: 'New Blog'}, );
 });
 
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {title: '404'}, );
 });
